84a9400b127ddc0c065b5a4fe5ee52b67e4a617e
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-01-20 15:10:54 -0800

    Fetching data from Api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Navbar from "./components/Navbar";
 import {Switch,Route} from "react-router-dom";
+import {fetchData} from "./Api";
 
 function App() {
+    useEffect( ()=>{
+        async function FetchData(){
+            const data = await fetchData();
+            console.log(data);
+        }
+       FetchData();
+
+    },[]);
   return (
        <div>
            <Switch>
