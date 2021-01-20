70c399ed2968e6cd5626d6c125d07833ef6cc061
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-01-19 21:18:29 -0800

    Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import { makeStyles } from '@material-ui/core/styles';
+import {Button ,
+    ClickAwayListener,
+    Grow ,
+    Paper ,
+    Popper ,
+    MenuItem,
+ MenuList,
+makeStyles} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
