4566720ccfc78c1056ab5397cf6526f1f2d6c196
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-01-19 19:00:39 -0800

    navbar changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,42 @@
-import React from "react";
-import {Link} from 'react-router-dom';
-import {useState} from 'react';
-import './navbar.sass';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default function Navbar() {
-    const [search, setSearch] = useState('');
-    return(
-        <div className='navbar'>
-            <Link className = 'logo' to='/'>
-               <h1>Covid Tracker</h1>
-            </Link>
-            <input id='search' type="text" placeholder="Search..." onChange={event =>{setSearch(event.target.value)}}/>
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
+export default function ButtonAppBar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+
+                    <Typography variant="h6" className={classes.title}>
+                        CovidInfo
+                    </Typography>
+
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
