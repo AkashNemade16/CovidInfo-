de42432c9c94e71d826f17c86c250d4e842dadc1
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-02-26 16:34:09 -0800

    appbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import {Typography, AppBar, Toolbar, Grid} from '@material-ui/core';
+
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
+    header: {
+        backgroundColor:'lightgreen',
+        color:"inherit",
+
     },
     title: {
-        flexGrow: 1,
-    },
+        flexGrow:1
+    }
+
 }));
 
-export default function Header() {
-    const classes = useStyles();
 
+const Header = props => {
+    const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <AppBar position="static">
+        <div>
+            <AppBar position="sticky" justify="center"
+                    className={classes.header}>
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title}>
-                      CovidInfo
+                    <Typography className={classes.title} variant="h6" color='inherit' >
+                       CovidInfo
                     </Typography>
                 </Toolbar>
             </AppBar>
         </div>
     );
-}
+};
+
+export default Header;
